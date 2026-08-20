f312cdb8c27c2887e84cb7747c39ba427e8ad8c5
Brian Maitner <bmaitner@usf.edu>
2026-08-20 14:49:56 -0400

    vectorized branch length rescaling to improve speed

--- R/scale_branches_multidimensional_distance.R
+++ R/scale_branches_multidimensional_distance.R
@@ -1,4 +1,3 @@
-
 ##########
 
 #'Scale the branches of a phylogeny according to phenotypic change.
@@ -15,7 +14,6 @@
 #' }
 #' @export
 #' @import Rphylopars
-#' @importFrom "stats" "dist"
 scale_branches_multidimensional <- function(tree,
                                           traits,
                                           rate = FALSE,
@@ -38,30 +36,29 @@ scale_branches_multidimensional <- function(tree,
     
     row.names(anc_recon)[1:length(tree$tip.label)] <- 1:length(tree$tip.label)
   
-      output_branches <- matrix(data=NA,nrow=length(tree$edge.length),ncol = 1)
       tree_x <- tree
       
-      for(i in 1:length(tree$edge.length)){
-
-        node_1 <- tree$edge[i,][1]
-        node_2 <- tree$edge[i,][2]
-        
-        value_1 <-anc_recon[which(row.names(anc_recon)==node_1),]
-        value_2 <-anc_recon[which(row.names(anc_recon)==node_2),]
-        
-        bl <- dist(rbind(value_1,value_2))[1]
-        
-        if(rate){
-          
-        bl<- (bl/tree$edge.length[i])
-          
-        }
-        
-        output_branches[i]<-bl
-        
-      }#i loop
+      # Branch lengths are the Euclidean distance in trait space between the two
+      # nodes subtending each edge.  Done in a single vectorized step: looking the
+      # nodes up one edge at a time is quadratic in tree size.
       
-      tree_x$edge.length <- output_branches
+      node_ids <- as.integer(row.names(anc_recon))
+      
+      row_1 <- match(tree$edge[,1], node_ids)
+      row_2 <- match(tree$edge[,2], node_ids)
+      
+      squared_change <- (anc_recon[row_1, , drop = FALSE] -
+                           anc_recon[row_2, , drop = FALSE])^2
+      
+      output_branches <- sqrt(rowSums(squared_change))
+      
+      if(rate){
+        
+        output_branches <- output_branches/tree$edge.length
+        
+      }
+      
+      tree_x$edge.length <- unname(output_branches)
       
       # Append trait data if requested
       
